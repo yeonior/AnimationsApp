9af162e188872f4549ab193b182693763fb3ac13
Rus <de.mr44@gmail.com>
2021-09-21 14:15:56 +0500

    Added movement animation

--- MyAnimations/Controllers/BasicAnimations.swift
+++ MyAnimations/Controllers/BasicAnimations.swift
@@ -10,13 +10,24 @@ import UIKit
 final class BasicAnimations: UIViewController {
     
     private var animationType: AnimationType? = nil
+    
+    private let square: UILabel = {
+        let label = UILabel()
+        label.frame = CGRect(x: 100, y: 100, width: 100, height: 100)
+        label.backgroundColor = .red
+        label.clipsToBounds = true
+        label.layer.cornerRadius = 16
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .green
+        view.backgroundColor = .systemBackground
+        configureAnimation(with: animationType)
     }
     
+    // MARK: - Init
     init(animationType: AnimationType) {
         super.init(nibName: nil, bundle: nil)
         self.animationType = animationType
@@ -24,5 +35,19 @@ final class BasicAnimations: UIViewController {
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Animation configuring
+    private func configureAnimation(with type: AnimationType?) {
+        guard let animationType = type else { return }
+        
+        view.addSubview(square)
+        
+        switch animationType {
+        case .movement:
+            UIView.animate(withDuration: 2.0) {
+                self.square.center = self.view.center
+            }
+        }
     }
 }
